1802fff9f8349fd492238c2cae0b51ad0eb008b0
ericlangTuring <ericlang80@gmail.com>
2026-03-29 20:56:42 -0300

    laxing cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,13 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    const frontendUrls = (process.env.FRONTEND_URLS || 'http://localhost:5173')
-        .split('|')
-        .map(url => url.trim());
+    //const frontendUrls = (process.env.FRONTEND_URLS || 'http://localhost:5173')
+    //    .split('|')
+     //   .map(url => url.trim());
 
     app.enableCors({
-        origin: frontendUrls,
-        credentials: true,
+        origin: '*',
     });
 
     await app.listen(process.env.PORT ?? 3000);
--- apps/frontend/src/apolloClient.ts
+++ apps/frontend/src/apolloClient.ts
@@ -3,7 +3,6 @@ import {ApolloClient, HttpLink, InMemoryCache} from '@apollo/client';
 const client = new ApolloClient({
   link: new HttpLink({
     uri: import.meta.env.VITE_API_URL,
-    credentials: 'include',
     headers: {
       'Content-Type': 'application/json',
     },
